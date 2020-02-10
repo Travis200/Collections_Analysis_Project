be7c3294156c802e2599ab8f41162ebbc78e06bb
b9029429 <Glasspane12>
2020-02-10 15:23:21 +0000

    Created array.

--- ArrayDirectory.java
+++ ArrayDirectory.java
@@ -1,5 +1,12 @@
 public class ArrayDirectory {
-    public static void main(String[] args) {
 
+    public static String[] arrayCreator() {
+        String[] arrayOfEntries = {"a", "b", "c"};
+        return arrayOfEntries;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(arrayCreator());
     }
 }
