9521af3d9c227f1c8524d02bbcd907a08c97f624
Travis Higgins <t.higgins1@newcastle.ac.uk>
2020-02-14 09:50:34 +0000

    Created Array Directory 2

--- ArrayDirectory.java
+++ ArrayDirectory.java
@@ -1,4 +1,36 @@
-public class ArrayDirectory {
+import java.util.List;
+
+public class ArrayDirectory implements Directory{
+    @Override
+    public void insertEntry(Entry entry) {
+
+    }
+
+    @Override
+    public void deleteEntryUsingName(String surname) {
+
+    }
+
+    @Override
+    public void deleteEntryUsingExtension(String number) {
+
+    }
+
+    @Override
+    public void updateExtensionUsingName(String surname, String newNumber) {
+
+    }
+
+    @Override
+    public String lookupExtension(String surname) {
+        return null;
+    }
+
+    @Override
+    public List<Entry> toArrayList() {
+        return null;
+    }
+
     public static void main(String[] args) {
 
     }
