620530f60e368791db336f8207dbc8310fedc011
b9029429 <Glasspane12>
2020-02-17 20:41:37 +0000

    Array directory complete.

--- ArrayDirectory.java
+++ ArrayDirectory.java
@@ -8,39 +8,38 @@ public class ArrayDirectory implements Directory {
 
     @Override
     public void insertEntry(Entry entry) {
-        //Does not work
         ArrayList<Entry> directoryEntriesArrayList = new ArrayList<Entry>(Arrays.asList(directoryEntries));
         directoryEntriesArrayList.add(entry);
-        this.directoryEntries = new Entry[directoryEntriesArrayList.size()];
-        this.directoryEntries = directoryEntriesArrayList.toArray(this.directoryEntries);
-        //System.out.println(directoryEntries);
+        directoryEntries = new Entry[directoryEntriesArrayList.size()];
+        directoryEntries = directoryEntriesArrayList.toArray(directoryEntries);
     }
 
     @Override
     public void deleteEntryUsingName(String surname) {
+        int lengthOfDE2 = ((directoryEntries.length)-1);
+        Entry[] directoryEntries2 = new Entry[lengthOfDE2];
         ArrayList<Entry> directoryEntriesArrayList = new ArrayList<Entry>(Arrays.asList(directoryEntries));
         for (int i = 0; i < directoryEntriesArrayList.size(); i++) {
-            //System.out.println(directoryEntriesArrayList.get(i).getSurname());
             if (directoryEntriesArrayList.get(i).getSurname().equals(surname)) {
-                //System.out.println();
                 directoryEntriesArrayList.remove(i);
             }
-            while(directoryEntriesArrayList.remove(null));
-            this.directoryEntries = directoryEntriesArrayList.toArray(this.directoryEntries);
-
         }
-        //System.out.println(Arrays.toString(directoryEntries));
+        directoryEntries2 = directoryEntriesArrayList.toArray(directoryEntries2);
+        directoryEntries = directoryEntries2;
     }
 
     @Override
     public void deleteEntryUsingExtension(String number) {
+        int lengthOfDE2 = ((directoryEntries.length)-1);
+        Entry[] directoryEntries2 = new Entry[lengthOfDE2];
         ArrayList<Entry> directoryEntriesArrayList = new ArrayList<Entry>(Arrays.asList(directoryEntries));
         for (int i = 0; i < directoryEntriesArrayList.size(); i++) {
             if (directoryEntriesArrayList.get(i).getTelephoneExtension().equals(number)) {
                 directoryEntriesArrayList.remove(i);
             }
-            this.directoryEntries = directoryEntriesArrayList.toArray(this.directoryEntries);
         }
+        directoryEntries2 = directoryEntriesArrayList.toArray(directoryEntries2);
+        directoryEntries = directoryEntries2;
     }
 
     @Override
@@ -48,11 +47,11 @@ public class ArrayDirectory implements Directory {
         ArrayList<Entry> directoryEntriesArrayList = new ArrayList<Entry>(Arrays.asList(directoryEntries));
         for (int i = 0; i < directoryEntriesArrayList.size(); i++) {
             if (directoryEntriesArrayList.get(i).getSurname().equals(surname)) {
-                //Insert code here
+                directoryEntriesArrayList.get(i).setTelephoneExtension(newNumber);
+
             }
-            this.directoryEntries = directoryEntriesArrayList.toArray(this.directoryEntries);
+            directoryEntries = directoryEntriesArrayList.toArray(directoryEntries);
         }
-        //System.out.println(Arrays.toString(directoryEntries));
     }
 
     @Override
@@ -61,39 +60,31 @@ public class ArrayDirectory implements Directory {
         String result = "";
         System.out.println();
         for (int i = 0; i < directoryEntriesArrayList.size(); i++) {
-            //System.out.println(i);
-            if (directoryEntriesArrayList.get(i).getTelephoneExtension().equals(surname)) {
+            if (directoryEntriesArrayList.get(i).getSurname().equals(surname)) {
                 System.out.println("Hello");
                 result = directoryEntriesArrayList.get(i).getTelephoneExtension();
             }
         }
-        //System.out.println(Arrays.toString(directoryEntries));
         return result;
     }
 
     @Override
     public List<Entry> toArrayList() {
-        List<Entry> thing = Arrays.asList(directoryEntries);
-        //System.out.println(Arrays.toString(directoryEntries));
-        return thing;
+        List<Entry> arrayList = Arrays.asList(directoryEntries);
+        return arrayList;
     }
 
     public static void main(String[] args) {
         ArrayDirectory obj1 = new ArrayDirectory();
         Entry entry1 = new Entry("Higgins", "T.S.H", "11111");
-        Entry entry2 = new Entry("Waterhouse","L.W","22222");
+        Entry entry2 = new Entry("Waterhouse","L.W.W","22222");
         Entry entry3 = new Entry("Mclaughlin", "M.M.M", "33333");
         obj1.insertEntry(entry1);
         obj1.insertEntry(entry2);
         obj1.insertEntry(entry3);
-        obj1.deleteEntryUsingName("Higgins");
-        //obj1.lookupExtension("Higgins");
-        //obj1.updateExtensionUsingName("Higgins", "01205");
-        //System.out.println(directoryEntries.length);
+        obj1.updateExtensionUsingName("Higgins", "44444");
         for(int i=0;i<directoryEntries.length;i++){
-            //System.out.println(directoryEntries.length);
-            //System.out.println(i);
-            System.out.println(directoryEntries[i].getSurname());
+            System.out.println(directoryEntries[i].getTelephoneExtension());
         }
 
     }
--- Input.java
+++ Input.java
@@ -1,40 +1,31 @@
 import java.io.BufferedReader;
-        import java.io.FileReader;
-        import java.io.IOException;
-        import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Input {
     public Object[] readFile() throws IOException {
         ArrayList<Entry> fileDataArrayList = new ArrayList<Entry>();
-        //Entry[] fileData = null;
         String row;
         BufferedReader csvReader = new BufferedReader(new FileReader("test_data_small.csv"));
+        System.out.println(1);
         while ((row = csvReader.readLine()) != null) {
+            System.out.println(2);
             String[] data = row.split(",");
-            System.out.println("this");
-            System.out.println(data[0]);
-            //System.out.println(data);
             Entry fileLine = new Entry(data[0], data[1], data[2]);
-            //System.out.println(Arrays.toString(data));
             fileDataArrayList.add(fileLine);
-            //System.out.println(fileLine);
+            System.out.println(3);
         }
-        //System.out.println(fileDataArrayList);
-        Entry test;
         Entry[] fileData = (Entry[]) fileDataArrayList.toArray();
-        //System.out.println(fileData);
+        System.out.println(3);
         csvReader.close();
+        System.out.println(4);
         return fileData;
         }
 
         public static void main (String[]args) throws IOException {
             Input obj = new Input();
-            obj.readFile();
-    /*public static void main(String[] args) throws IOException{
-        ReadFile test = new ReadFile();
-        System.out.println((Arrays.toString(test.readFile())));
-    }*/
+            System.out.println(obj.readFile());
         }
     }
 
